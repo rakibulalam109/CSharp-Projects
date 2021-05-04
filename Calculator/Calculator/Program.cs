e7b7f2705ed76275d765be920a31a4d4ce87195f
rakibulalam109 <rakibulalam109@gmail.com>
2021-05-05 00:59:48 +0600

    Calculator Console Application Added

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t----Simple Calculator----");
+            Console.WriteLine("Operations You Can Do Here( + , - , * , / , % )");
+            Console.WriteLine(@"Type ""end"" For Exit ");
+
+
+            while (true)
+            {
+                
+                Console.Write("Enter Your First Number: ");
+                double firstNumber = double.Parse(Console.ReadLine());
+
+                Console.Write("Select Operation( + , - , * , / , % ): ");
+                string operation = Console.ReadLine();
+
+                Console.Write("Enter Your Second Number: ");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                double result = 0;
+                if (operation == "+")
+                {
+                    result = firstNumber + secondNumber;
+                }
+                else if (operation == "-")
+                {
+                    result = firstNumber - secondNumber;
+                }
+                else if (operation == "*")
+                {
+                    result = firstNumber * secondNumber;
+                }
+                else if (operation == "/")
+                {
+                    result = firstNumber / secondNumber;
+                }
+                else if (operation == "%")
+                {
+                    result = firstNumber % secondNumber;
+                }
+
+                Console.WriteLine($"Result For {firstNumber}{operation}{secondNumber} is : {result}");
+                Console.WriteLine(@"If if want to continue press ""Enter"" button,\n" +
+                    @"If you want to exit type ""end"" ");
+                if (Console.ReadLine() == "end")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
